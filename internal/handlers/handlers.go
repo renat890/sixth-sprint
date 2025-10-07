e66cb658fd20952bb2ea6b86f15a966023514f62
Renat Sedlovsky <rensedlovskij911@gmail.com>
2025-10-07 10:50:23 +0500

    fix: исправление замечаний

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,79 +2,66 @@ package handlers
 
 import (
 	"bytes"
-	"errors"
 	"io"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
-	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
 )
 
 func GetIndex(w http.ResponseWriter, r *http.Request) {
-	var buff bytes.Buffer
-
-	file, err := os.Open("index.html")
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			http.Error(w, os.ErrNotExist.Error(), http.StatusNotFound)
-			return
-		}
-		http.Error(w, err.Error(), http.StatusBadRequest)
+	if r.Method == "GET" {
+		http.ServeFile(w, r, "index.html")
+		w.Header().Set("Content-type", "text/html")
+		w.WriteHeader(http.StatusOK)
+	} else {
+		http.Error(w, "Недопустимый метод", http.StatusMethodNotAllowed)
 	}
-
-	_, err = io.Copy(&buff, file)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	
-	w.Header().Set("Content-type", "text/html")
-	w.WriteHeader(http.StatusOK)
-	w.Write(buff.Bytes())
 }
 
 func PostForm(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseMultipartForm(10 << 20)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+	if r.Method == "POST" {	
+		err := r.ParseMultipartForm(10 << 20)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
-	file, _, err := r.FormFile("myFile")
-	if err != nil {
-		http.Error(w, "ошибка при получении файла", http.StatusBadRequest)
-		return
-	}
-	defer file.Close()
+		file, _, err := r.FormFile("myFile")
+		if err != nil {
+			http.Error(w, "ошибка при получении файла", http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
 
-	var data bytes.Buffer
-	_, err = io.Copy(&data, file)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	
-	var str string
-	if service.IsMorse(data.String()) {
-		str = morse.ToText(data.String())
+		var data bytes.Buffer
+		_, err = io.Copy(&data, file)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		str := service.Answer(data.String())
+
+		localFile, err := os.OpenFile(time.Now().UTC().String(), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		_, err = localFile.Write([]byte(str))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-type", "text/plain; charset=UTF-8")
+		_, err = w.Write([]byte(str))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	} else {
-		str = morse.ToMorse(data.String())
+		http.Error(w, "Недопустимый метод", http.StatusMethodNotAllowed)
 	}
-
-	localFile, err := os.OpenFile(time.Now().UTC().String(), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	_, err = localFile.Write([]byte(str))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-type", "text/plain")
-	w.Write([]byte(str))
-
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,8 +1,21 @@
 package service
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
 
 func IsMorse(text string) bool { 
 	return strings.TrimRight(text, ".- ") == ""
 }
-	
+
+func Answer(data string) string {
+	var str string
+	if IsMorse(data) {
+		str = morse.ToText(data)
+	} else {
+		str = morse.ToMorse(data)
+	}
+	return str
+}
